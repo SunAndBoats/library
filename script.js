aec74754cb717f0995edd13681e25f2296f7d2d6
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-04-02 13:07:24 +1300

    remove btn and toggle btn logic

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ function Book(title, author, pages, read){
     this.pages=pages;
     this.read=read;
 }
+
+Book.prototype.toggleRead=function(){
+    this.read=!this.read
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead()
+    render()
+}
+
 function render(){
     let libraryBook=document.querySelector("#library")
     libraryBook.innerHTML = ""; //  Limpiar antes de agregar los libros
@@ -19,12 +29,19 @@ function render(){
             <h3>Author: ${book.author}</h3>
             <h3>Nº pages: ${book.pages}</h3>
             <h3>Estado: ${book.read ? "✅ Leído" : "❌ No leído"}</h3>
+            <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+            <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
             </div>
         `
+        
         libraryBook.appendChild(bookEl);
     }
 }
 
+function removeBook(index){
+    myLibrary.splice(index,1)
+    render()
+}
 
 
 function addBookToLibrary(){
@@ -36,12 +53,18 @@ function addBookToLibrary(){
     console.log(newBook)
     myLibrary.push(newBook)
     render()
-   newBookForm.style.display = "none"
+    let newBookForm=document.querySelector("#new-book-form")
+    newBookForm.style.display = "none"
 
 }
 render()
 let newBookBtn=document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click",function(){
+     // Limpiar los valores del formulario
+     document.querySelector("#title").value = "VOTE ME(usted)";
+     document.querySelector("#author").value = "";
+     document.querySelector("#pages").value = "";
+     document.querySelector("#read").checked = false;
    
    let newBookForm=document.querySelector("#new-book-form")
  newBookForm.style.display="block"
